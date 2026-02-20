1b459f7628c7dd9c9a22775735060611ccd86e75
Vitor Augusto <vitorlealeng@gmail.com>
2026-02-20 08:46:16 -0300

    reply-to

--- src/07-request-reply/client.ts
+++ src/07-request-reply/client.ts
@@ -2,10 +2,10 @@ import amqp from "amqplib";
 import crypto from "crypto";
 
 async function toUpperCaseRPC(text: string): Promise<string> {
-  const connection = await amqp.connect("amqp://admin:admin@localhost:5672");
+  const connection = await amqp.connect("amqp://user:password@localhost:5672");
   const channel = await connection.createChannel();
 
-  const q = await channel.assertQueue("", { exclusive: true });
+  const q = await channel.assertQueue("", { exclusive: true }); //fila exclusiva para resposta
   await channel.assertQueue("rpc_queue");
   const correlationId = crypto.randomBytes(16).toString("hex");
 
@@ -13,20 +13,28 @@ async function toUpperCaseRPC(text: string): Promise<string> {
     channel.consume(
       q.queue,
       (msg) => {
+        console.log(
+          "[x] Recebida resposta com correlationId %s",
+          msg?.properties.correlationId,
+        );
         if (msg && msg.properties.correlationId === correlationId) {
           resolve(msg.content.toString());
           setTimeout(() => connection.close(), 500);
         }
       },
-      { noAck: true }
-    ); 
+      { noAck: true },
+    );
 
+    //envia mensagem
+    console.log(
+      "[x] Enviando requisição RPC com correlationId %s",
+      correlationId,
+    );
     channel.sendToQueue("rpc_queue", Buffer.from(text), {
       correlationId,
       replyTo: q.queue,
     });
   });
 }
-
 
 toUpperCaseRPC("mensagem simples").then(console.log);
--- src/07-request-reply/server.ts
+++ src/07-request-reply/server.ts
@@ -1,11 +1,11 @@
 import amqp from "amqplib";
 
 async function startRPCServer() {
-  const connection = await amqp.connect("amqp://admin:admin@localhost:5672");
+  const connection = await amqp.connect("amqp://user:password@localhost:5672");
   const channel = await connection.createChannel();
 
   await channel.assertQueue("rpc_queue");
-  
+
   //work queue
   channel.prefetch(1);
 
@@ -15,6 +15,8 @@ async function startRPCServer() {
     if (msg) {
       const input = msg.content.toString();
       const result = input.toUpperCase();
+
+      console.log('[x] server "%s", respondendo com "%s"', input, result);
 
       channel.sendToQueue(msg.properties.replyTo, Buffer.from(result), {
         correlationId: msg.properties.correlationId,
